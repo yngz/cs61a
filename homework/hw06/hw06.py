44bdccbedbf922f591afba4260c325cb1cc6d36f
Yang Zou <yang0201@outlook.com>
2018-10-08 23:30:34 +0100

    complete hw06 Fib

---
+++ homework/hw06/hw06.py
@@ -0,0 +1,76 @@
+# Object Oriented Programming
+
+class Fib():
+    """A Fibonacci number.
+
+    >>> start = Fib()
+    >>> start
+    0
+    >>> start.next()
+    1
+    >>> start.next().next()
+    1
+    >>> start.next().next().next()
+    2
+    >>> start.next().next().next().next()
+    3
+    >>> start.next().next().next().next().next()
+    5
+    >>> start.next().next().next().next().next().next()
+    8
+    >>> start.next().next().next().next().next().next() # Ensure start isn't changed
+    8
+    """
+
+    def __init__(self, value=0):
+        self.value = value
+
+    def next(self):
+        if self.value == 0:
+            next_fib = Fib(1)
+        else:
+            next_fib = Fib(self.prev + self.value)
+        next_fib.prev = self.value
+        return next_fib
+
+    def __repr__(self):
+        return str(self.value)
+
+class VendingMachine:
+    """A vending machine that vends some product for some price.
+
+    >>> v = VendingMachine('candy', 10)
+    >>> v.vend()
+    'Machine is out of stock.'
+    >>> v.deposit(15)
+    'Machine is out of stock. Here is your $15.'
+    >>> v.restock(2)
+    'Current candy stock: 2'
+    >>> v.vend()
+    'You must deposit $10 more.'
+    >>> v.deposit(7)
+    'Current balance: $7'
+    >>> v.vend()
+    'You must deposit $3 more.'
+    >>> v.deposit(5)
+    'Current balance: $12'
+    >>> v.vend()
+    'Here is your candy and $2 change.'
+    >>> v.deposit(10)
+    'Current balance: $10'
+    >>> v.vend()
+    'Here is your candy.'
+    >>> v.deposit(15)
+    'Machine is out of stock. Here is your $15.'
+
+    >>> w = VendingMachine('soda', 2)
+    >>> w.restock(3)
+    'Current soda stock: 3'
+    >>> w.restock(3)
+    'Current soda stock: 6'
+    >>> w.deposit(2)
+    'Current balance: $2'
+    >>> w.vend()
+    'Here is your soda.'
+    """
+    "*** YOUR CODE HERE ***"
