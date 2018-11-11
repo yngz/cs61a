8a8a8569ef518bc58c460133603437b58b050b13
Yang Zou <yang0201@outlook.com>
2018-11-11 22:49:19 +0000

    start hw10

---
+++ homework/hw10/hw10.scm
@@ -0,0 +1,16 @@
+(define (accumulate combiner start n term)
+  'YOUR-CODE-HERE
+)
+
+(define (accumulate-tail combiner start n term)
+  'YOUR-CODE-HERE
+)
+
+(define (partial-sums stream)
+  'YOUR-CODE-HERE
+  (helper 0 stream)
+)
+
+(define (rle s)
+  'YOUR-CODE-HERE
+)
